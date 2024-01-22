af96e164bbad5adb8afe8696914179bad6f60e8f
Noman Rafiq <heyimnomee@gmail.com>
2024-01-22 11:48:48 +0500

    handling errors

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express();
 const tasks = require('./routes/tasksRoute');
 require('dotenv').config();
 const connectDB = require('./db/connect');
-
+const notFound = require('./middlewares/not-found');
+const errorHandler = require('./middlewares/error');
 //middlewares
 app.use(express.static('./public'));
 app.use(express.json());
 
 //routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound); //not found error page
 
+app.use(errorHandler); // if no page is found that the last handler
 //app.get('/api/v1/tasks') - get all tasks
 //app.post('/api/v1/tasks') - create a new task
 //app.get('/api/v1/tasks/:id') - get single task
--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,124 +1,84 @@
 const TaskModel = require('../dbModels/TaskModel');
+const asyncWrapper = require('../middlewares/async');
 
-const getAllTasks = async (req, res) => {
-    try {
-        const allTasks = await TaskModel.find({});
-        // return res.status(200).json(allTasks);
-        // return res.status(200).json({
-        //     success: true,
-        //     data: {
-        //         nbHits: allTasks.length, //nbHits: Number of Hits
-        //         allTasks,
-        //     },
-        // });
-        return res.status(200).json({
-            status: 'success',
-            data: {
-                nbHits: allTasks.length, //nbHits: Number of Hits
-                allTasks,
-            },
-        });
-    } catch (err) {
-        return res.status(500).json({ message: 'Error Retreiving Tasks' });
+const getAllTasks = asyncWrapper(async (req, res, next) => {
+    const allTasks = await TaskModel.find();
+    return res.status(200).json({
+        status: 'success',
+        data: {
+            nbHits: allTasks.length, //nbHits: Number of Hits
+            allTasks,
+        },
+    });
+});
+
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await TaskModel.create(req.body);
+    return res.status(201).json({
+        status: 'success',
+        data: {
+            task,
+        },
+    });
+});
+
+const getTask = asyncWrapper(async (req, res, next) => {
+    const taskID = req.params.id;
+    const task = await TaskModel.findOne({
+        _id: taskID,
+    });
+    if (!task) {
+        const error = new Error('Not Found');
+        error.status = 404;
+        return next(error);
+        //we will get this error if the ID format is correct but the ID is not avaialble
+        //if the id syntax is right but we cannot find the data, throw this error
+        return res.status(404).json({ msg: `No task with id: ${taskID}` });
     }
-};
+    return res.status(201).json({
+        status: 'success',
+        data: {
+            task,
+        },
+    });
+});
 
-const createTask = async (req, res) => {
-    try {
-        const task = await TaskModel.create(req.body);
-        return res.status(201).json(task);
-    } catch (err) {
-        return res.status(500).json({ message: 'Server Error' });
+const delTask = asyncWrapper(async (req, res) => {
+    const taskID = req.params.id;
+    const task = await TaskModel.findOneAndDelete({ _id: taskID });
+    if (!task) {
+        return res.status(404).json({ message: `No task with that ${taskID}` });
     }
-};
+    return res.status(200).json({
+        status: 'success',
+        data: null,
+    });
+});
 
-const getTask = async (req, res) => {
-    try {
-        const taskID = req.params.id;
-        const task = await TaskModel.findOne({
-            _id: taskID,
-        });
-        if (!task) {
-            //we will get this error if the ID format is correct but the ID is not avaialble
-            //if the id syntax is right but we cannot find the data, throw this error
-            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+const updateTask = asyncWrapper(async (req, res) => {
+    const taskID = req.params.id;
+    //for findOneand Update we will need id to filter and data to update
+    const task = await TaskModel.findOneAndUpdate(
+        { _id: taskID }, //taskID
+        //data to update aka Old data
+        req.body,
+        {
+            new: true,
+            runValidators: true,
         }
-        return res.status(200).json({ task });
-    } catch (err) {
-        // we will get this error if the ID is correct but the server isn't connecting or the ID format is incorrect
-        return res.status(500).json(err);
+    );
+
+    if (!task) {
+        return res.status(404).json({ message: `No task with that ${taskID}` });
     }
-};
 
-const delTask = async (req, res) => {
-    try {
-        const taskID = req.params.id;
-        const task = await TaskModel.findOneAndDelete({ _id: taskID });
-        if (!task) {
-            return res
-                .status(404)
-                .json({ message: `No task with that ${taskID}` });
-        }
-        return res.status(200).json({ task });
-        // return res.status(200).send();
-        // return res.status(200).json({ task: null, status: 'success' });
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-};
-
-const updateTask = async (req, res) => {
-    try {
-        const taskID = req.params.id;
-        //for findOneand Update we will need id to filter and data to update
-        const task = await TaskModel.findOneAndUpdate(
-            { _id: taskID }, //taskID
-            //data to update aka Old data
-            req.body,
-            {
-                new: true,
-                runValidators: true,
-            }
-        );
-
-        if (!task) {
-            return res
-                .status(404)
-                .json({ message: `No task with that ${taskID}` });
-        }
-
-        return res.status(200).json({ id: taskID, data: req.body });
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-};
-
-// const editTask = async (req, res) => {
-//     try {
-//         const taskID = req.params.id;
-//         //for findOneand Update we will need id to filter and data to update
-//         const task = await TaskModel.findOneAndUpdate(
-//             { _id: taskID }, //taskID
-//             //data to update aka Old data
-//             req.body,
-//             {
-//                 new: true,
-//                 runValidators: true,
-//                 overwrite: true,
-//             }
-//         );
-
-//         if (!task) {
-//             return res
-//                 .status(404)
-//                 .json({ message: `No task with that ${taskID}` });
-//         }
-
-//         return res.status(200).json({ id: taskID, data: req.body });
-//     } catch (err) {
-//         return res.status(500).json(err);
-//     }
-// };
+    return res.status(200).json({
+        status: 'success',
+        data: {
+            task,
+        },
+    });
+});
 
 module.exports = {
     getAllTasks,
